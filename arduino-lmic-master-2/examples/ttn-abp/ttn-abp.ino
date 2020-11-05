2dc207c2dcd0fab94b059eb5ac967db4a29cd421
mariasepulveda <61802755+mariasepulveda@users.noreply.github.com>
2020-11-05 22:29:40 +0100

    Actualizado tiempo de envio

--- arduino-lmic-master-2/examples/ttn-abp/ttn-abp.ino
+++ arduino-lmic-master-2/examples/ttn-abp/ttn-abp.ino
@@ -61,7 +61,7 @@ static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 15;
+const unsigned TX_INTERVAL = 480;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
